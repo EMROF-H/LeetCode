021cf74fb19cac6ba476ab2c9a6abe54d036b075
EMROF <393575404@qq.com>
2022-08-13 11:05:17 +0800

    证明

---
+++ 873.最长的斐波那契子序列的长度.cs
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=873 lang=csharp
+ *
+ * [873] 最长的斐波那契子序列的长度
+ */
+
+// @lc code=start
+public class Solution {
+    public int LenLongestFibSubseq(int[] arr) {
+
+    }
+}
+// @lc code=end
